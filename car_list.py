94b24e2d7cab835aca957725ef45d8d0c4420ef0
wrangman <wrangman@gmail.com>
2022-12-08 23:53:03 +0100

    vs

--- car_list.py
+++ car_list.py
@@ -1,45 +1,61 @@
 import os
+import time
 from Cars import MyCars
 from bcolors import bcolors
 
 car = MyCars()
 
-data = car.load_from_file("cars.json")
-car.cars = data
-os.system('cls')
+load_stored_cars = car.load_from_file("cars.json")
 
+if load_stored_cars:                                #If there are stored cars add them
+    car.cars = load_stored_cars    
+    
+os.system('cls')
 
 print(bcolors.YELLOW + """
 FAVORITBILAR ---------------------------------------------------
 - Lägg till bil genom att skriva märke och modell med mellanslag
-- Ta bort genom att skriva respektive nummer.
-- Avsluta genom att skriva "/q"
-----------------------------------------------------------------""" + bcolors.DEFAULT)
-car.list_cars()
+- Ta bort genom att skriva respektive nummer
+- Spara & avsluta genom att skriva "/q"
+----------------------------------------------------------------""" + bcolors.CYAN)
+
+if len(car.cars) > 0: 
+    car.list_cars() 
+else: 
+    print(bcolors.CYAN + "(Inga sparade bilar hittades)")
 
 while True:
     print(bcolors.YELLOW + "-" * 64)
-    entry = input(bcolors.CYAN + "Lägg till bil (märke & modell): " + bcolors.DEFAULT)
+    entry = input(bcolors.DEFAULT + "Lägg till bil (märke & modell): " + bcolors.CYAN)
 
     if entry.upper() == "/Q": 
+        print(bcolors.DEFAULT + "Sparar din billista...")
+        time.sleep(1)
         car.save_to_file("cars.json")
-        print(bcolors.DEFAULT)
         exit()
     
     if entry.isdigit():
-        car.remove_car(int(entry)-1)
+        total_cars = len(car.cars)
+        
+        if int(entry) >= 1 and int(entry) < total_cars + 1:
+            car.remove_car(int(entry)-1)
+        
+        else:
+            print(bcolors.RED + f"Bilen finns inte - skriv ett nummer mellan 1 och {total_cars}")
+            time.sleep(1)
+    
     else:
-        try:
+        if not " " in entry:
+            print(bcolors.RED + "Felaktig inmatning - skriv märke & modell")
+            time.sleep(1)
+    
+        else:                                           # add new car                                   
             make, model = entry.split(" ",1)
             car.add_car(make, model)
-
-        except ValueError:
-            print(bcolors.RED + "Felaktig inmatning - försök igen.")
-            continue
         
     os.system('cls')
 
-    print(bcolors.DEFAULT)
+    print(bcolors.CYAN)
     car.list_cars()
     
     
