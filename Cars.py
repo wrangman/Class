8a91c4fe75019d142cb76b77051ddd30e60db075
wrangman <wrangman@gmail.com>
2022-12-15 12:33:51 +0100

    102

--- Cars.py
+++ Cars.py
@@ -2,7 +2,7 @@
 CARS.PY: Bilklass
 
 __author__  = "Johan Wrangö"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __email__   = "johan.wrango@ntig.se"
 '''
 
@@ -23,12 +23,15 @@ class MyCars():
     def remove_car(self, index):
         del self.cars[index]
         
-    def save_to_file(self, filename):                    # Saves to JSON
+    def sort_cars(self):                                    # Sort the list of cars in place by make and model
+        self.cars.sort(key=lambda car: (car["make"], car["model"]))
+        
+    def save_to_file(self, filename):                       # Saves to JSON
         with open(filename, 'w') as f:
             json_string = json.dumps(self.cars)
             f.write(json_string)
         
-    def load_from_file(self, filename):                  # Loads the cars from a JSON file
+    def load_from_file(self, filename):                     # Loads the cars from a JSON file
         try:
             with open(filename, "r") as file:
                 return json.load(file)
--- car_list.py
+++ car_list.py
@@ -2,7 +2,7 @@
 CAR_LIST.PY: Dina favoritbilar :)
 
 __author__  = "Johan Wrangö"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __email__   = "johan.wrango@ntig.se"
 '''
 
@@ -12,18 +12,20 @@ from Cars import MyCars
 from bcolors import bcolors
 
 car = MyCars()
+auto_sort = False
 
 stored_cars = car.load_from_file("cars.json")  #Load stored cars from file - the function is in Cars.py
 
 if stored_cars:                                #If there are stored cars add them
     car.cars = stored_cars    
-    
+
 os.system('cls')
 
 print(bcolors.YELLOW + """
 FAVORITBILAR ===================================================            
 - Lägg till bil genom att skriva märke och modell med mellanslag
 - Ta bort genom att skriva respektive nummer
+- Aktivera/deaktivera sorteringsfunktion: /sort
 - Spara & avsluta genom att skriva "/q"
 ================================================================""" + bcolors.CYAN)
 
@@ -37,11 +39,21 @@ while True:
     print(bcolors.YELLOW + "-" * 64)
     entry = input(bcolors.DEFAULT + "Lägg till bil (märke & modell): " + bcolors.CYAN)
 
-    if entry.upper() == "/Q": 
+    if entry.upper() == "/Q" or entry.upper() == "/QUIT": 
         print(bcolors.DEFAULT + "Sparar din billista...")
         time.sleep(1)
         car.save_to_file("cars.json")
         exit()
+        
+    if entry.upper() == "/S" or entry.upper() == "/SORT":
+        os.system('cls')
+        car.sort_cars()
+        car.list_cars()
+        if auto_sort:
+            auto_sort = False
+        else:
+            auto_sort = True
+        continue
     
     if entry.isdigit():
         total_cars = len(car.cars)
@@ -65,6 +77,7 @@ while True:
         
     os.system('cls')
     print(bcolors.CYAN)
+    if auto_sort: car.sort_cars()                   # if you sort the cars they will automatically sort until you disable with /s
     car.list_cars()
     
     
