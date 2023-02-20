f1929cf465168675050fbffc6cc0298a9e73a923
wrangman <wrangman@gmail.com>
2023-02-20 10:32:19 +0100

    sdf

---
+++ lists.py
@@ -0,0 +1,18 @@
+import os
+
+
+os.system('cls')
+mylist = ["joe","bro", "sho", "foe"]    #variabel lista
+
+myword = "school"
+
+for i in mylist:
+   print(i)
+
+print("hur många ord i listna", len(mylist))
+
+print(len(myword))
+
+
+
+
